d433908318db469c6bf2e5a33c1df572a946b6fa
Amin Babar <amin.babar1@gmail.com>
2021-09-06 15:23:42 -0400

    session controller and user controller complete

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,32 @@
+class Api::SessionsController < ApplicationController
+
+    # # login form
+    # def new
+    # end
+
+    # login
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if @user
+            login(@user)
+            # QUESTION WHY
+            render "api/users/user"
+        else
+            render json: ["Invalid username or password"], status: 401
+        end
+
+    end
+
+    # log a user out
+    def destroy
+        @user = current_user
+        if @user
+            logout
+            render "api/users/user"
+        else
+            render json: ["not logged in"], status: 404
+        end
+    end
+
+end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,29 @@
 class Api::UsersController < ApplicationController
-    
+
+
+    # def new
+    # end
+
+    # CHANGE line 12 render json: @user!!!
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages
+        end
+
+    end
+
+
+    # def destroy
+    # end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :fname, :lname, :birthday, :city, :work)
+    end
+
 end
