3f3b45e8241db6f8a8f25736172cf7e55a58cdcf
Amin Babar <amin.babar1@gmail.com>
2021-09-06 13:26:44 -0400

    done with user model

---
+++ db/migrate/20210906163827_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :fname, null: false
+      t.string :lname, null: false
+      t.string :email, null: false
+      t.datetime :birthday, null: false
+      t.string :city
+      t.string :work
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+
+      t.timestamps
+    end
+    add_index :users, :email, unique:true
+    add_index :users, :session_token, unique:true
+  end
+end
