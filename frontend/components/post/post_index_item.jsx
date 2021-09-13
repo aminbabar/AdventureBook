bb430266983c85f6cc30077fe385cdeaba094fc7
Amin Babar <amin.babar1@gmail.com>
2021-09-12 23:37:01 -0400

    fleshed out the post components

--- frontend/components/post/post_index_item.jsx
+++ frontend/components/post/post_index_item.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 
+
+// Post header
+
+
+
 const PostIndexItem = (props) => {
     return (
-        <li className={"post-body"} key={props.post.id}>
-            {props.post.body}
-        </li>
+
+        <div className="post">
+            <div className="post-header">
+                <div className="post-header-photo">
+
+                </div>
+
+                <div className="post-header-middle">
+
+                </div>
+
+                <div className="post-header-dropdown">
+
+                </div>
+            </div>
+
+            <div className="post-body">
+
+            </div>
+
+
+            <div className="post-likes">
+
+            </div>
+
+
+            <div className="post-buttons">
+
+            </div>
+
+            <div className="post-comments">
+
+            </div>
+        </div>
+
+
+
     );
 };
 
 export default PostIndexItem;
+
+
+
+// <li className={"post-body"} key={props.post.id}>
+//     {props.post.body}
+// </li>
