9836a5b9bf0485463c18ea218c562e39686c8192
Amin Babar <amin.babar1@gmail.com>
2021-09-14 00:47:02 -0400

    finished setting up the modal

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,9 +6,12 @@ import LoginFormContainer from "./session_form/login_form_container";
 import { AuthRoute, ProtectedRoute} from "../utils/route_util";
 import NewsFeedIndex from "./newsfeed/news_feed_index";
 
+import Modal from "modal/modal";
+
 const App = () => {
     return(
         <div>
+            <Modal />
             <ProtectedRoute exact path="/" component={NewsFeedIndex}/>
             <AuthRoute path="/signup" component={SignupContainer}/>
             <AuthRoute path="/login" component={LoginFormContainer}/>
---
+++ frontend/components/modal/modal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { closeModal } from "../../actions/modal_actions";
+import { connect } from "react-redux";
+
+const Modal = ({modal, closeModal}) => {
+    if (!modal) {
+        return null;
+    }
+    let component;
+    switch (modal) {
+        case "create_post":
+            // component = <CreatePostContainer here!!    
+            break;
+        default:
+            return null;
+    };
+
+    return (
+        <div className="modal-background" onClick={closeModal}>
+            <div className="modal-child" onClick={(e) => e.stopPropagation()}>
+                {component}
+            </div>
+        </div>
+    );
+};
+
+const mstp = (state) => {
+    return ({
+        modal: state.ui.modal
+    });
+};
+
+const mdtp = (dispatch) => {
+    return({
+        closeModal: () => dispatch(closeModal())
+    });
+};
+
+export default connect(mstp, mdtp)(Modal);
