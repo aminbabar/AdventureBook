b1c85da537ecf2ea0a3ba54bc88eca34cf729496
Amin Babar <amin.babar1@gmail.com>
2022-01-11 01:31:16 -0500

    divided login form into more divs for ease with styling

--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -35,41 +35,63 @@ class LoginForm extends React.Component {
         return (
             <div className="login-form-container">
 
-                <div>
+                <div className="splash-logo-div">
+                    <div className="splash-logo">
+                        Workoutbook
+                    </div>
                     {/* <img src="https://lh3.googleusercontent.com/proxy/HtU0a_h_wjaViEyAiiCU3ivY2-ullrROdSDpRlfACis1w3ds0ntElc7oj7NXhlh3cjLIg03ymIi2AaX7Kh0VjgHzUinANqwkOeD0UvCE-eZwo8JEq1TjJus3n9SJUXHG0vFptjuZuIR7Iqz7SUm3LErP" alt="image tag" /> */}
-                    Connect with friends and the world around you on AlgoBook
+
+                    <div className="splash-text">
+                        Connect with friends and the world around you on AlgoBook
+                    </div>
                 </div>
+
                 <form className="login-form">
                     <ul>
                         {this.props.errors.map((error, i) => {
                             return (<li key={`error-${i}`}>{error}</li>)
                         })}
                     </ul>
-                    <label>
-                        <input 
-                            type="text"
-                            value={this.state.email}
-                            placeholder="Email or Phone Number"
-                            onChange={this.handleInput("email")}
-                        />
-                    </label>
+
+                    <div className="text-password">
+                        <label>
+                            <input 
+                                type="text"
+                                value={this.state.email}
+                                placeholder="Email or Phone Number"
+                                onChange={this.handleInput("email")}
+                            />
+                        </label>
 
 
-                    <label>
-                        <input
-                            type="password"
-                            value={this.state.password}
-                            placeholder="Password"
-                            onChange={this.handleInput("password")}
-                        />
-                    </label>
+                        <label>
+                            <input
+                                type="password"
+                                value={this.state.password}
+                                placeholder="Password"
+                                onChange={this.handleInput("password")}
+                            />
+                        </label>
+                    </div>
 
-                    <button 
-                        className="login-button" 
-                        onClick={this.handleSubmit}>
-                            Log In
-                    </button>
-                    <Link className="signup-link" to={"/signup"}>Create New Account</Link>
+                    <div className="login-button-div">
+                        <button
+                            className="login-button" 
+                            onClick={this.handleSubmit}>
+                                Log In
+                        </button>
+                    </div>
+                    
+                    <div className="">
+                        <a></a>
+                    </div>
+
+
+                    <div className="signup-link-div">
+                        <Link className="signup-link" to={"/signup"}>Create New Account</Link>
+                    </div>
+
+
                 </form>
             </div>
         );
