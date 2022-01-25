6c1505b0042a5407c711f7fbe00e4f75fc96efd3
Amin Babar <amin.babar1@gmail.com>
2022-01-24 19:01:00 -0500

    dropdown.jsx file improved by drying out some of the code

--- frontend/components/nav_bar/dropdown.jsx
+++ frontend/components/nav_bar/dropdown.jsx
@@ -8,23 +8,30 @@ class Dropdown extends React.Component {
             show: false
         };
         this.changeState = this.changeState.bind(this);
-        this.closeDropdown = this.closeDropdown.bind(this);
+        // this.closeDropdown = this.closeDropdown.bind(this);
     };
 
     changeState() {
-        let newState = !this.state.show;
-        this.setState({show: newState});
+        // let newState = !this.state.show;
+        // this.setState({show: newState});
+
+        this.setState((prevState) => {
+            return { show: !prevState.show }
+        });
     };
 
-    closeDropdown() {
-        this.setState({show: false});
-    };
+    // closeDropdown() {
+    //     this.setState((prevState) => {
+    //         return {show: !prevState.show}
+    //     });
+    //     // this.setState({show: false});
+    // };
 
 
     render() {
         let cName = this.state.show ? "dropdown-show" : "dropdown-hide";
         return (
-            <button className="dropdown-container" onClick={this.changeState} onBlur={this.closeDropdown}>
+            <button className="dropdown-container" onClick={this.changeState} onBlur={this.changeState}>
                 <div className="dropdown-container-icon"> {this.props.icon}</div>
                 
                 <ul className={cName} onClick={e => e.stopPropagation()}>
