8b283570d90fa043c08d9df9ae8712c8fbde2cec
Amin Babar <amin.babar1@gmail.com>
2022-05-09 18:23:30 -0400

    connected edit profile to state

--- frontend/components/profile/edit_profile.jsx
+++ frontend/components/profile/edit_profile.jsx
@@ -6,31 +6,40 @@ class EditProfile extends React.Component {
         super(props);
         const {user} = this.props;
         this.state = {
-            fname: user.fname,
-            lname: user.lname,
-            city: user.city,
-            education: user.education,
-            work: user.work,
-            portfolio: user.portfolio
+            fname: user.fname || "",
+            lname: user.lname || "",
+            city: user.city || "",
+            education: user.education || "",
+            work: user.work || "",
+            portfolio: user.portfolio || ""
         };
     }
 
+    update(field) {
+        return(e) => {
+            this.setState({[field]: e.currentTarget.value})
+        };
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        debugger;
+    }
 
     render() {
         const {user, updateUser} = this.props;
-        // debugger;
         return(
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h1>Edit profile</h1>
 
                 <label>
                     First Name
-                    <input type="text" value={this.state.fname}></input>
+                    <input type="text" value={this.state.fname} onChange={this.update("fname")} />
                 </label>
 
                 <label>
                     Last Name
-                    <input type="text" value={this.state.lname}></input>
+                    <input type="text" value={this.state.lname} onChange={this.update("lname")} />
                 </label>
 
                 <label>
@@ -40,22 +49,22 @@ class EditProfile extends React.Component {
                 
                 <label>
                     City
-                    <input type="text" value={this.state.city}></input>
+                    <input type="text" value={this.state.city} onChange={this.update("city")} />
                 </label>
 
                 <label>
                     Education
-                    <input type="text" value={this.state.education}></input>
+                    <input type="text" value={this.state.education} onChange={this.update("education")} />
                 </label>
 
                 <label>
                     Work
-                    <input type="text" value={this.state.work}></input>
+                    <input type="text" value={this.state.work} onChange={this.update("work")} />
                 </label>
 
                 <label>
                     Portfolio Website
-                    <input type="text" value={this.state.portfolio}></input>
+                    <input type="text" value={this.state.portfolio} onChange={this.update("portfolio")} />
                 </label>
 
                 <input type="submit" value="Update Profile"/>
