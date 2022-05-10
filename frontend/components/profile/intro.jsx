0c1886eff29efa33c1910d7a53a41e0f2cdd1295
Amin Babar <amin.babar1@gmail.com>
2022-05-10 01:21:35 -0400

    fixed issue on safari with undefined

--- frontend/components/profile/intro.jsx
+++ frontend/components/profile/intro.jsx
@@ -11,35 +11,39 @@ class Intro extends React.Component {
     };
 
     email() {
-        const {email} = this.props.user;
+        const email = this.props.user?.email;
+
         if (email) {
             return (<div>Email me at {email}</div>)
         };
     }
 
     work() {
-        const { work } = this.props.user;
+        const work = this.props.user?.work;
+
         if (work) {
             return (<div>Works at {work}</div>)
         };
     }
 
     city() {
-        const { city } = this.props.user;
+        const city = this.props.user?.city;
         if (city) {
-            return (<div>Lives in {city}</div>)
+            return (<div>Lives in {this.props.user.city}</div>)
         };
     }
 
     education() {
-        const { education } = this.props.user;
+        const education = this.props.user?.education;
+
         if (education) {
             return (<div>Studied at {education}</div>)
         };
     }
 
     portfolio() {
-        const { portfolio } = this.props.user;
+        const portfolio = this.props.user?.portfolio;
+
         if (portfolio) {
             return (<div>Here's my portfolio website {portfolio}</div>)
         };
