7661ffc625518c3652d9580303e10d44f30f46dd
Amin Babar <amin.babar1@gmail.com>
2022-06-14 21:37:02 -0400

    likes reducer complete

---
+++ frontend/reducers/likes_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_LIKE, REMOVE_LIKE } from "../actions/like_actions";
+
+const LikesReducer = (oldState={}, action) => {
+    Object.freeze(oldState);
+
+    let newState = Object.assign({}, oldState);
+
+    switch (action.type) {
+        case RECEIVE_LIKE:
+            newState[action.like.id] = action.like;
+            return newState;
+        case REMOVE_LIKE:
+            delete newState[action.likeId];
+            return newState;
+        default:
+            return oldState;
+    };
+};
+
+export default LikesReducer;
