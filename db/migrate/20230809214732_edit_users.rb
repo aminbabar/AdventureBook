a21436947c45b9f54731fb22b7293d6418afeca0
Amin Babar <amin.babar1@gmail.com>
2023-09-19 12:01:34 -0400

    fixed bug with migrations

--- db/migrate/20230809214732_edit_users.rb
+++ db/migrate/20230809214732_edit_users.rb
@@ -1,7 +1,7 @@
 class EditUsers < ActiveRecord::Migration[6.1]
   def change
-    rename_column :users, :first_name, :first_name
-    rename_column :users, :last_name, :last_name
+    rename_column :users, :fname, :first_name
+    rename_column :users, :lname, :last_name
     add_column :users, :gender, :string, null: false
     add_column :users, :birthday, :date, null: false
   end
