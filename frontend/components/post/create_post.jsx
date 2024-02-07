83555eb2d705ef2c01e24a9254133de134983474
Amin Babar <amin.babar1@gmail.com>
2024-02-07 11:53:27 -0500

    create post container styling

--- frontend/components/post/create_post.jsx
+++ frontend/components/post/create_post.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
 import { openModal } from "../../actions/modal_actions";
+import { MdOutlineVideoCameraFront } from "react-icons/md";
+import { GrGallery } from "react-icons/gr";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const CreatePost = (props) => {
     return(
         <div className="create-post-top">
-            <div className="create-post-container" onClick={() => props.openModal("create_post")}>
+            <div className="create-post-container" >
                 <div className="image-container">
-                    <img src={props.currentUser.profilePhoto} />
+                    <Link to={`/users/${props.currentUser.id}`}>
+                        <img src={props.currentUser.profilePhoto} />
+                    </Link>
                 </div>
 
-                <div className="create-post-text">
+                <div className="create-post-text" onClick={() => props.openModal("create_post")}>
                     {`What's on your mind, ${props.currentUser.first_name}?`}
                 </div>
 
             </div>
-            <div className="hr-container">
-                <div className="hr"></div>
+            <div className="hr"></div>
+
+            <div className="create-post-buttons">
+                <div onClick={() => props.openModal("create_post")}><MdOutlineVideoCameraFront /></div>
+                <div onClick={() => props.openModal("create_post")}><GrGallery /></div>
             </div>
         </div>
 
